fb2a60bcb1e855e9042d151893ec8a6cf1e9744a
Stefan Kirsch <stefankirsch@posteo.de>
2025-01-10 16:28:24 +0100

    Add a banner

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ source("round_app.R")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   titlePanel("Round-app: Round Students' marks"),
+  # add a banner
   tags$div(
     style = "background-color: #FFA500; color: black; padding: 10px; text-align: center; font-weight: bold; font-size: 16px; margin-bottom: 15px;",
     "Warning: For demonstration purposes only. Do not upload any personal (student) data!"
